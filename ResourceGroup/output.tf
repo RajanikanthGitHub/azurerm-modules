e8c25fe38e3b650ac39aba969161ff2b4f14e520
Rajanikanth86GitHub <rajanikanth86@gmail.com>
2021-07-19 21:30:54 +0100

    updated variables

--- ResourceGroup/output.tf
+++ ResourceGroup/output.tf
@@ -5,3 +5,11 @@ output "Resource_group_name" {
 output "Resource_group_location" {
   value = azurerm_resource_group.test-rsg.location
 }
+
+output "project_name" {
+  value = var.project_name
+}
+
+output "rsg_name" {
+  value = var.rsg_name
+}
--- vnet/vnet.tf
+++ vnet/vnet.tf
@@ -4,7 +4,7 @@ module "rsg" {
 
 # creating virtual network
 resource "azurerm_virtual_network" "test-vnet" {
-  name                = "${var.project_name}-${var.vnet_name}"
+  name                = "${module.rsg.project_name}-${var.vnet_name}"
   location            = module.rsg.Resource_group_location 
   resource_group_name = module.rsg.Resource_group_name
   address_space       = ["${var.address_space}"]
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "test-vnet" {
 
 # Creating Subnet for Virtual network
 resource "azurerm_subnet" "test-subnet" {
-  name                 = "${var.project_name}-${var.vnet_name}-${var.subnet_name[count.index]}"
+  name                 = "${module.rsg.project_name}-${var.vnet_name}-${var.subnet_name[count.index]}"
   count                = length(var.subnet_name)
   resource_group_name  = module.rsg.Resource_group_name
   virtual_network_name = azurerm_virtual_network.test-vnet.name
